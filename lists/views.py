52c88da13d994d151916cac2057aa32a94f54c30
miletskiy <s.miletskiy@gmail.com>
2015-10-21 13:09:54 +0300

    Add new model List. Migrations, slightly change the tests.

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 # from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 def home_page(request):
@@ -8,7 +8,7 @@ def home_page(request):
 	# 	return HttpResponse(request.POST['item_text'])
 	# return render(request, 'home.html')
 	# return HttpResponse('<html><title>To-Do lists</title></html>')
-	
+
 	# if request.method == 'POST':
 	# 	Item.objects.create(text=request.POST['item_text'])
 	# 	return redirect('/lists/the-only-list-in-the-world/')
@@ -19,6 +19,7 @@ def view_list(request):
 	return render(request,'list.html', {'items': items})
 
 def new_list(request):
-	Item.objects.create(text=request.POST['item_text'])
+	list_ = List.objects.create()
+	Item.objects.create(text=request.POST['item_text'], list=list_)
 	return redirect('/lists/the-only-list-in-the-world/')
 
