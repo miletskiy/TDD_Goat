7ca4dcf041d4c68c71b0303bdb016c19d0e4f3ad
miletskiy <s.miletskiy@gmail.com>
2015-11-04 21:52:32 +0200

    Rename all item input ids(id_new_item to id_text) and names(item_text to text). Still broken.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -9,7 +9,8 @@ class ItemValidationTest(FunctionalTest):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        self.get_item_input_box().send_keys('\n')
+        # self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank.
@@ -17,11 +18,13 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item!")
 
         # She tries again with some text for the  item, which now works
-        self.browser.find_element_by_id('id_new_item').send_keys('Buy one bottle of milk\n')
+        self.get_item_input_box().send_keys('Buy one bottle of milk\n')
+        # self.browser.find_element_by_id('id_new_item').send_keys('Buy one bottle of milk\n')
         self.check_for_row_in_list_table('1: Buy one bottle of milk')
 
         # Perversely, she now decides to submit a second blank list item
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        self.get_item_input_box().send_keys('\n')
+        # self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # She receives a similar warning on the list page
         self.check_for_row_in_list_table('1: Buy one bottle of milk')
@@ -29,7 +32,8 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, "You can't have an empty list item!")
 
         # And she can correct it by filling some text in
-        self.browser.find_element_by_id('id_new_item').send_keys('Make cup of tea\n')
+        self.get_item_input_box().send_keys('Make cup of tea\n')
+        # self.browser.find_element_by_id('id_new_item').send_keys('Make cup of tea\n')
         self.check_for_row_in_list_table('1: Buy one bottle of milk')
         self.check_for_row_in_list_table('2: Make cup of tea')
 
